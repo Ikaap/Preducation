3cdb0f04e454c40273b0f6cdd068a4d39c6e77da
Hamidatun Nisa <hamidatunnisa9d@gmail.com>
2023-11-29 00:03:23 +0700

    Finished layout OTP Success Dialog

---
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/DialogSuccesOtpActivity.kt
@@ -0,0 +1,11 @@
+package com.kelompoksatuandsatu.preducation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DialogSuccesOtpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialog_succes_otp)
+    }
+}
