39ab582e8bd82cee96a81f78e515c6f04a7dbe0f
Hamidatun Nisa <hamidatunnisa9d@gmail.com>
2023-11-28 22:56:36 +0700

    Finished layout OTP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // otp
+    implementation("com.github.appsfeature:otp-view:1.0")
+
     // coil
     implementation("io.coil-kt:coil:2.4.0")
     // rv
