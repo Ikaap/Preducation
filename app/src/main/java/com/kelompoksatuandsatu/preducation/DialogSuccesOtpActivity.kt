ef88f11644d81674669741789363f1a56ecc6b3c
Hamidatun Nisa <hamidatunnisa9d@gmail.com>
2023-11-29 23:03:37 +0700

    Finished layout input OTP, Toast Button Success and Failed input OTP, Dialog Success OTP

--- app/src/main/java/com/kelompoksatuandsatu/preducation/DialogSuccesOtpActivity.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/DialogSuccesOtpActivity.kt
@@ -1,7 +1,7 @@
 package com.kelompoksatuandsatu.preducation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class DialogSuccesOtpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
