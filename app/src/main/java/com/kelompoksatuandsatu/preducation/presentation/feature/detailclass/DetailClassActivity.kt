82774bcca24c646c5145648cb80b352f231f0615
Ikap <ikapurwanti144@gmail.com>
2023-11-30 05:32:23 +0700

    slicing activity detail class, add tab layout for detail class, add fragment about and curriculcum, slicing dialog buy class

--- app/src/main/java/com/kelompoksatuandsatu/preducation/presentation/feature/detailclass/DetailClassActivity.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/presentation/feature/detailclass/DetailClassActivity.kt
@@ -1,12 +1,67 @@
 package com.kelompoksatuandsatu.preducation.presentation.feature.detailclass
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.kelompoksatuandsatu.preducation.R
+import com.kelompoksatuandsatu.preducation.databinding.ActivityDetailClassBinding
+import com.kelompoksatuandsatu.preducation.presentation.feature.detailclass.adapter.ViewPagerAdapter
 
 class DetailClassActivity : AppCompatActivity() {
+
+    private val binding: ActivityDetailClassBinding by lazy {
+        ActivityDetailClassBinding.inflate(layoutInflater)
+    }
+
+    private val adapterViewPager: ViewPagerAdapter by lazy {
+        ViewPagerAdapter(supportFragmentManager, lifecycle)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_class)
+        setContentView(binding.root)
+
+        setLayoutViewPager()
+        setBottomSheet()
+    }
+
+    private fun setBottomSheet() {
+
+    }
+
+    private fun setLayoutViewPager() {
+        with(binding) {
+            tabLayout.addTab(tabLayout.newTab().setText("About"))
+            tabLayout.addTab(tabLayout.newTab().setText("Curriculcum"))
+
+            viewPager.adapter = adapterViewPager
+
+            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                @SuppressLint("UseCompatLoadingForDrawables")
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    if (tab != null) {
+                        viewPager.currentItem = tab.position
+                        tab?.view?.background = getDrawable(R.drawable.selected_tab_background)
+                    }
+                }
+
+                @SuppressLint("UseCompatLoadingForDrawables")
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                    tab?.view?.background = getDrawable(R.drawable.unselected_tab_background)
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                }
+            })
+
+            viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    tabLayout.selectTab(tabLayout.getTabAt(position))
+                }
+            })
+        }
     }
 }
