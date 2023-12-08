c8b9239d2d599010c186ec07f5bfa099b144cc3e
nellamchya <88300976+nellamchya@users.noreply.github.com>
2023-12-08 21:48:53 +0700

    API Notification

---
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/model/notification/NotificationItem.kt
@@ -0,0 +1,8 @@
+package com.kelompoksatuandsatu.preducation.model.notification
+
+data class NotificationItem(
+    val _id: Int,
+    val userId: Int,
+    val title: String,
+    val description: String
+)
