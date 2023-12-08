33fcf89aad2b502df67919cebe32015c00e97c50
nellamchya <88300976+nellamchya@users.noreply.github.com>
2023-12-08 22:58:07 +0700

    Show data in Recycle View Notification

--- app/src/main/java/com/kelompoksatuandsatu/preducation/model/notification/NotificationItem.kt
+++
@@ -1,8 +0,0 @@
-package com.kelompoksatuandsatu.preducation.model.notification
-
-data class NotificationItem(
-    val _id: Int,
-    val userId: Int,
-    val title: String,
-    val description: String
-)
