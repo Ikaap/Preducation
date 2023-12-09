755bde30d1d23912b4b67736ea30e228af0c0c02
L200210273ResyaLusiara <l200210273@student.ums.ac.id>
2023-12-10 05:30:20 +0700

    Fix AndroidManifest.xml

--- app/src/test/java/com/example/login/ExampleUnitTest.kt
+++ app/src/test/java/com/example/login/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
 package com.example.login
 
-import org.junit.Assert.*
 import org.junit.Test
+
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
