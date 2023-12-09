fae9d5d2d7fd751b08d6d63bf7e01548156e7d2b
L200210273ResyaLusiara <l200210273@student.ums.ac.id>
2023-12-10 05:59:06 +0700

    Config data binding

--- app/src/androidTest/java/com/example/login/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/login/ExampleInstrumentedTest.kt
@@ -1,13 +1,5 @@
 package com.example.login
 
-<<<<<<< HEAD
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
-import org.junit.Test
-import org.junit.runner.RunWith
-
-=======
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -16,7 +8,6 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
->>>>>>> ca19fd6 (Fix AndroidManifest.xml)
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -30,8 +21,4 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.login", appContext.packageName)
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ca19fd6 (Fix AndroidManifest.xml)
