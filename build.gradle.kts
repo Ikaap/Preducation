13e731684154b531b5ad3b12ca51a2ba86d3bb2d
L200210273ResyaLusiara <l200210273@student.ums.ac.id>
2023-12-10 05:30:20 +0700

    Fix AndroidManifest.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.0")
@@ -5,10 +6,15 @@ buildscript {
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
     }
 }
+=======
+>>>>>>> 7b6c0f8 (Initial Commit)
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+<<<<<<< HEAD
     id("androidx.navigation.safeargs.kotlin") version "2.7.2" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
+=======
+>>>>>>> 7b6c0f8 (Initial Commit)
 }
