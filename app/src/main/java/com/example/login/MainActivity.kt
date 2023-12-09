db5177ecdefbde94b2379e1adb5395925a810e38
L200210273ResyaLusiara <l200210273@student.ums.ac.id>
2023-12-10 05:59:06 +0700

    Config data binding

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.example.login
 
-<<<<<<< HEAD
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.kelompoksatuandsatu.preducation.R
-=======
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
->>>>>>> 1753cfb (Fix AndroidManifest.xml)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1753cfb (Fix AndroidManifest.xml)
