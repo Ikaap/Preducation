eba8e9eb72e92d8eacaa92c099de1e0923815889
L200210273ResyaLusiara <l200210273@student.ums.ac.id>
2023-12-10 11:24:44 +0700

    Consume auths Login API

--- app/src/main/java/com/kelompoksatuandsatu/preducation/App.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/App.kt
@@ -2,9 +2,14 @@ package com.kelompoksatuandsatu.preducation
 
 import android.app.Application
 import com.kelompoksatuandsatu.preducation.di.AppModules
+import com.kelompoksatuandsatu.preducation.network.AuthService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class App : Application() {
     override fun onCreate() {
@@ -17,6 +22,26 @@ class App : Application() {
             androidLogger()
             androidContext(this@App)
             modules(AppModules.modules)
+        }
+    }
+
+    companion object {
+        fun createAuthService(): AuthService {
+            val loggingInterceptor = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://preducation.up.railway.app")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            return retrofit.create(AuthService::class.java)
         }
     }
 }
--- app/src/main/java/com/kelompoksatuandsatu/preducation/di/AppModules.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/di/AppModules.kt
@@ -1,8 +1,14 @@
 package com.kelompoksatuandsatu.preducation.di
 
+import com.kelompoksatuandsatu.preducation.App.Companion.createAuthService
+import com.kelompoksatuandsatu.preducation.network.AuthService
 import org.koin.core.module.Module
+import org.koin.dsl.module
 
 object AppModules {
-
-    val modules: List<Module> = listOf()
+    val modules: List<Module> = listOf(
+        module {
+            single<AuthService> { createAuthService() }
+        }
+    )
 }
