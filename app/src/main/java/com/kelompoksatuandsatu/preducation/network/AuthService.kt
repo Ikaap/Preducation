c4204884781ce9471d936083ef47fa9202e247d7
L200210273ResyaLusiara <l200210273@student.ums.ac.id>
2023-12-10 11:24:44 +0700

    Consume auths Login API

--- app/src/main/java/com/kelompoksatuandsatu/preducation/network/AuthService.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/network/AuthService.kt
@@ -12,14 +12,5 @@ interface AuthService {
     fun login(
         @Field("identifier") identifier: String,
         @Field("password") password: String
-    ): Call<LoginResponse>
-
-    @FormUrlEncoded
-    @POST("/api/v1/auths/register")
-    fun register(
-        @Field("name") name: String,
-        @Field("email") email: String,
-        @Field("phone") phone: String,
-        @Field("password") password: String
-    ): Call<RegisterResponse>
+    ): Call<APIResponse>
 }
