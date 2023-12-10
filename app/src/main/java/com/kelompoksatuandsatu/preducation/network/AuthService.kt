42e4c0958b54b1cd2da21bdbe4147b1d574552ea
L200210273ResyaLusiara <l200210273@student.ums.ac.id>
2023-12-10 12:02:08 +0700

    Consume auths Register API

--- app/src/main/java/com/kelompoksatuandsatu/preducation/network/AuthService.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/network/AuthService.kt
@@ -1,11 +1,6 @@
 package com.kelompoksatuandsatu.preducation.network
 
-<<<<<<< HEAD
-import com.kelompoksatuandsatu.preducation.model.LoginResponse
-import com.kelompoksatuandsatu.preducation.model.RegisterResponse
-=======
 import com.kelompoksatuandsatu.preducation.model.APIResponse
->>>>>>> eba8e9e (Consume auths Login API)
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -17,18 +12,5 @@ interface AuthService {
     fun login(
         @Field("identifier") identifier: String,
         @Field("password") password: String
-<<<<<<< HEAD
-    ): Call<LoginResponse>
-
-    @FormUrlEncoded
-    @POST("/api/v1/auths/register")
-    fun register(
-        @Field("name") name: String,
-        @Field("email") email: String,
-        @Field("phone") phone: String,
-        @Field("password") password: String
-    ): Call<RegisterResponse>
-=======
     ): Call<APIResponse>
->>>>>>> eba8e9e (Consume auths Login API)
 }
