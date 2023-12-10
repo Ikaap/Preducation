79643e2587fd31ff64a09fcee09240f4a3ccbea6
Ikap <ikapurwanti144@gmail.com>
2023-12-10 09:12:11 +0700

    update detail activity intent data from bottom sheet to payment activity

---
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/model/detailcourse/ChapterViewParam.kt
@@ -0,0 +1,14 @@
+package com.kelompoksatuandsatu.preducation.model.detailcourse
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ChapterViewParam(
+    val createdAt: String?,
+    val id: String?,
+    val isActive: Boolean?,
+    val title: String?,
+    val totalDuration: Int?,
+    val videos: List<VideoViewParam>?
+) : Parcelable
