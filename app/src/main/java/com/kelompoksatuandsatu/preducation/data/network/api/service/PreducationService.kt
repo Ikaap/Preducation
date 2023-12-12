0001c42d65b445df0fbf0585f3e3c702c626377a
Ikap <ikapurwanti144@gmail.com>
2023-12-12 12:15:53 +0700

    update auth feature

--- app/src/main/java/com/kelompoksatuandsatu/preducation/data/network/api/service/PreducationService.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/data/network/api/service/PreducationService.kt
@@ -21,6 +21,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -74,7 +75,7 @@ interface PreducationService {
     @GET("api/v1/payments")
     suspend fun getHistoryPayment() // : HistoryPaymentResponse
 
-    //     auth
+//     auth
     @POST("api/v1/auths/register")
     suspend fun userRegister(@Body userRegisterRequest: RegisterRequest): RegisterResponse
 
