9a829c18a48669e017ea8ae7a36e764e35b6861e
Hamidatun Nisa <hamidatunnisa9d@gmail.com>
2023-12-15 15:36:51 +0700

    Added navigate to detail and completed search bar and see all

--- app/src/main/java/com/kelompoksatuandsatu/preducation/presentation/feature/otp/OtpActivity.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/presentation/feature/otp/OtpActivity.kt
@@ -3,14 +3,19 @@ package com.kelompoksatuandsatu.preducation.presentation.feature.otp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kelompoksatuandsatu.preducation.R
+import com.kelompoksatuandsatu.preducation.databinding.ActivityOtpBinding
 import com.otpview.OTPListener
 import com.otpview.OTPTextView
 import io.github.muddz.styleabletoast.StyleableToast
 
 class OtpActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityOtpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otp)
+        binding = ActivityOtpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val otpTextView = findViewById(R.id.otp_view) as OTPTextView
         otpTextView.requestFocusOTP()
