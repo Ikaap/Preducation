8ded81f218f5d53ff0268050a9afaba8932f117f
L200210273ResyaLusiara <l200210273@student.ums.ac.id>
2023-12-16 17:56:48 +0700

    Fix history payment api

--- app/src/main/java/com/kelompoksatuandsatu/preducation/data/network/api/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/data/network/api/interceptor/AuthInterceptor.kt
@@ -16,7 +16,7 @@ class AuthInterceptor(private val preference: UserPreferenceDataSource) : Interc
                     .build()
             } else {
                 request = request.newBuilder()
-                    .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc5YTI3MzQ3NjEwMDFmOWU0OGRmNGEiLCJlbWFpbCI6InVzZXIxQGV4YW1wbGUuY29tIiwicGhvbmUiOiIxMjM0NTY3ODkwIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MDI3MjIxNDUsImV4cCI6MTcwMjk4MTM0NX0.IqvbBr2aZbvJn0qxPap_C27rW-bW6U5E964ksypuU1E")
+                    .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc5YTI3MzQ3NjEwMDFmOWU0OGRmNGEiLCJlbWFpbCI6InVzZXIxQGV4YW1wbGUuY29tIiwicGhvbmUiOiIxMjM0NTY3ODkwIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MDI3MjQwNTgsImV4cCI6MTcwMjk4MzI1OH0.p66wL7hCgMrDfFwDTCWsoWSZXb7PiIhOPBv9zYEJREA")
                     .build()
             }
         }
