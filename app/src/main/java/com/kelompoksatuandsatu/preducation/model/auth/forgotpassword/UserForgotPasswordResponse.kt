e9c20ed2053262bb2d945c14c4b9a1c1819c0674
Ikap <ikapurwanti144@gmail.com>
2023-12-18 21:36:55 +0700

    finishing feature forgot password

---
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/model/auth/forgotpassword/UserForgotPasswordResponse.kt
@@ -0,0 +1,7 @@
+package com.kelompoksatuandsatu.preducation.model.auth.forgotpassword
+
+data class UserForgotPasswordResponse(
+    val success: Boolean,
+    val status: String?,
+    val message: String?
+)
