504f866f82620d6332d4234942e6cbf417c349a6
Ikap <ikapurwanti144@gmail.com>
2023-12-20 13:01:01 +0700

    update feature otp for show error message from response api

--- app/src/main/java/com/kelompoksatuandsatu/preducation/data/network/api/model/common/BaseResponse.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/data/network/api/model/common/BaseResponse.kt
@@ -3,8 +3,13 @@ package com.kelompoksatuandsatu.preducation.data.network.api.model.common
 import com.google.gson.annotations.SerializedName
 
 data class BaseResponse(
+
+    @SerializedName("message")
+    val message: String?,
+
     @SerializedName("status")
     val status: String?,
-    @SerializedName("message")
-    val message: String?
+
+    @SerializedName("success")
+    val success: Boolean?
 )
--- app/src/main/java/com/kelompoksatuandsatu/preducation/data/repository/UserRepository.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/data/repository/UserRepository.kt
@@ -5,16 +5,19 @@ import com.kelompoksatuandsatu.preducation.data.network.api.datasource.UserDataS
 import com.kelompoksatuandsatu.preducation.data.network.api.model.auth.forgotpassword.ForgotPasswordRequest
 import com.kelompoksatuandsatu.preducation.data.network.api.model.auth.login.LoginRequest
 import com.kelompoksatuandsatu.preducation.data.network.api.model.auth.otp.postemail.EmailOtpRequest
+import com.kelompoksatuandsatu.preducation.data.network.api.model.auth.otp.verifyotp.OtpRequest
+import com.kelompoksatuandsatu.preducation.data.network.api.model.auth.otp.verifyotp.toOtpResponse
 import com.kelompoksatuandsatu.preducation.data.network.api.model.auth.register.RegisterRequest
 import com.kelompoksatuandsatu.preducation.data.network.api.model.changepassword.ChangePasswordRequest
 import com.kelompoksatuandsatu.preducation.data.network.api.model.changepassword.toPasswordList
 import com.kelompoksatuandsatu.preducation.data.network.api.model.user.UserRequest
-import com.kelompoksatuandsatu.preducation.data.network.api.model.user.toUserViewParam
-import com.kelompoksatuandsatu.preducation.model.auth.OtpData
+import com.kelompoksatuandsatu.preducation.data.network.api.model.user.toUserList
 import com.kelompoksatuandsatu.preducation.model.auth.UserAuth
 import com.kelompoksatuandsatu.preducation.model.auth.UserLogin
 import com.kelompoksatuandsatu.preducation.model.auth.forgotpassword.UserForgotPassword
 import com.kelompoksatuandsatu.preducation.model.auth.otp.postemailotp.EmailOtp
+import com.kelompoksatuandsatu.preducation.model.auth.otp.verifyotp.OtpData
+import com.kelompoksatuandsatu.preducation.model.auth.otp.verifyotp.OtpResponse
 import com.kelompoksatuandsatu.preducation.model.user.Password
 import com.kelompoksatuandsatu.preducation.model.user.UserViewParam
 import com.kelompoksatuandsatu.preducation.utils.ResultWrapper
@@ -38,7 +41,7 @@ interface UserRepository {
 
     suspend fun postEmailOtp(request: EmailOtp): Flow<ResultWrapper<Boolean>>
 
-    suspend fun verifyOtp(request: OtpData): Flow<ResultWrapper<OtpResponse>>
+    suspend fun verifyOtp(request: OtpData): Flow<ResultWrapper<BaseResponse>>
 
     suspend fun userForgotPassword(request: UserForgotPassword): Flow<ResultWrapper<Boolean>>
 }
@@ -107,7 +110,7 @@ class UserRepositoryImpl(
         }
     }
 
-    override suspend fun verifyOtp(request: OtpData): Flow<ResultWrapper<OtpResponse>> {
+    override suspend fun verifyOtp(request: OtpData): Flow<ResultWrapper<BaseResponse>> {
         return proceedFlow {
             val otpRequest = OtpRequest(request.otp)
             val otpResult = userDataSource.verifyOtp(otpRequest)
