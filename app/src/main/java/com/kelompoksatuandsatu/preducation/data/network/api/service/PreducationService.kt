fbaed16e52b2bce8c4188f2e7930e749b2ad38fa
Ninda_Nuraisyah <nindanuraisyah0903@gmail.com>
2023-12-29 12:28:39 +0700

    finish filter by category

--- app/src/main/java/com/kelompoksatuandsatu/preducation/data/network/api/service/PreducationService.kt
+++ app/src/main/java/com/kelompoksatuandsatu/preducation/data/network/api/service/PreducationService.kt
@@ -60,6 +60,9 @@ interface PreducationService {
     @GET("api/v1/courses")
     suspend fun getCourseHome(@Query("category") category: List<String>? = null, @Query("typeClass") typeClass: String? = null): CourseResponse
 
+    @GET("api/v1/courses")
+    suspend fun getCourseHome(@Query("category") category: List<String>? = null, @Query("typeClass") typeClass: String? = null): CourseResponse
+
     // notification
     @GET("api/v1/notifications")
     suspend fun getUserNotification(): NotificationResponse
